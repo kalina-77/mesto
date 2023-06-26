047744a3936c6bd4732fdb33491392c1dffd8e8b
Sergey Kalinin <metrolog-service@yandex.ru>
2023-06-26 14:58:44 +0300

    Начало работы над ПР5

--- script/script.js
+++ script/script.js
@@ -1,17 +1,63 @@
-// 1. Открытие/закрытие окна и редактирования профиля пользователя
-
 // Задаем константы и переменные
 
 const editProfileButtonElement = document.querySelector('.profile__edit-button');
 const closeProfileButtonElement = document.querySelector('.popup__close-button');
 const popupElement = document.querySelector('.popup');
+const addPhotoButtonElement = document.querySelector('.profile__add-button');
+const closePhotoButtonElement = document.querySelector('.popup__close-button_photo');
+const photoEditElement = document.querySelector('.popup_add-photo');
 let userName = document.querySelector('.profile__name');
 let userDesc = document.querySelector('.profile__desc');
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__input_type_name');
 let descInput = document.querySelector('.popup__input_type_desc');
+const cardTemplate = document.querySelector('#card-photo').content;
+const photoElement = document.querySelector('.elements');
+const submitPhotoElement = document.querySelector('.popup__submit_photo');
+const placePhotoInput = document.querySelector('.popup__input_type_place');
+const placeNameInput = document.querySelector('.popup__input_type_link');
+const likePhotoElement = document.querySelectorAll('.elements__like');
 
-// Функция открытия-закрытия окна
+// ПР5
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// Создание шаблона и добавление 6 карточек при закрузке страницы
+
+initialCards.forEach(place => {
+  const cardElement = cardTemplate.querySelector('.elements__element').cloneNode(true);
+  cardElement.querySelector('.elements__image').src = place.link;
+  cardElement.querySelector('.elements__image').alt = place.name;
+  cardElement.querySelector('.elements__text').textContent = place.name;
+  photoElement.append(cardElement);
+});
+
+// Функция открытия-закрытия окна редактирования профиля пользователя
 
 function handleClick(e) {
   if (e.target === e.currentTarget) {
@@ -26,9 +72,11 @@ function handleClick(e) {
   }
 }
 
-// Функция отмены отправки формы, так как нет сервера
+// Функция изменения данных пользователя
 
 function handleFormSubmit(evt) {
+  // Отмена отправки формы на сервер
+
   evt.preventDefault();
 
   // Вставляем новые текстовые данные из формы в переменные
@@ -41,12 +89,43 @@ function handleFormSubmit(evt) {
   popupElement.classList.toggle('popup_opened');
 }
 
-// Обработчик формы (вызов функции)
+// Функция открытия/закрытия формы добавления фото
 
-formElement.addEventListener('submit', handleFormSubmit);
+function photoAdd() {
+  photoEditElement.classList.toggle('popup_opened');
+}
 
-// Обработчик открытия/закрытия окна (вызов функции)
+// Функция like/unlike
+
+document.querySelectorAll('.elements__like').forEach(likePhotoElement => {
+  likePhotoElement.addEventListener('click', () => {
+    likePhotoElement.classList.toggle('elements__like_active');
+  });
+});
+
+// Функция добавления фото
+
+function submitPhoto(evt) {
+  // Отмена отправки формы на сервер
+
+  evt.preventDefault();
+}
+
+// Обработчик открытия/закрытия окна редактирования данных пользователя
 
 editProfileButtonElement.addEventListener('click', handleClick);
 closeProfileButtonElement.addEventListener('click', handleClick);
 popupElement.addEventListener('click', handleClick);
+
+// Обработчик формы изменения данных пользователя
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+// Обработчик открытия/закрытия окна добавления фото
+
+addPhotoButtonElement.addEventListener('click', photoAdd);
+closePhotoButtonElement.addEventListener('click', photoAdd);
+
+// Обработчик добавления фото
+
+submitPhotoElement.addEventListener('click', submitPhoto);
